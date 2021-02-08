dd3a8cca723cee8502287edce23b6bea6a307903
Priyanga1311 <78338138+Priyanga1311@users.noreply.github.com>
2021-02-08 10:32:12 +0530

    repos file added

---
+++ repos/MVCTutorial/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using MVCTutorial.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCTutorial.Controllers
+{
+    public class TestController : Controller
+    {
+
+        // GET: Test
+        public ActionResult Index()
+        {
+            compileEntities db = new compileEntities();
+            Table2 class =  db.Table2();
+    }
+           
+       
+          
+            return View();
+}
+        }
+    }
---
+++ repos/Measurement/Models/EmployeeView.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Measurement.Models
+{
+    public class EmployeeView
+    {
+    }
+}
