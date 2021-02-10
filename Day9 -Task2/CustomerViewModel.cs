2143bea9613799862e4ba1609cb90fc038723248
Geethakrishna44 <78337829+Geethakrishna44@users.noreply.github.com>
2021-02-10 01:24:20 -0800

    Day9-Task2
    Task of day9 is completed and this the second task for us. I completed my mine what they gave

---
+++ Day9 -Task2/CustomerViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Day9.Models
+{
+    public class CustomerViewModel
+    {
+        public int CustomerID { get; set; }
+        public string CustomerName { get; set; }
+        public Nullable<int> Phoneno { get; set; }
+        public int MobileID { get; set; }
+
+        public string Mobiletype { get; set; }
+    }
+}
---
+++ Day9 -Task2/TestController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Day9.Models;
+
+namespace Day9.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+
+            sskEntities1 db = new sskEntities1();
+            List<Order> orderlist = db.Orders.ToList();
+            CustomerViewModel orderVM = new CustomerViewModel();
+            List<CustomerViewModel> orderVMList = orderlist.Select(x => new CustomerViewModel 
+            { 
+            CustomerName = x.CustomerName,
+             CustomerID = x.CustomerID,
+              Phoneno = x.Phoneno,
+               MobileID = x.MobileID,
+               Mobiletype = x.detail.Mobiletype }).ToList();
+
+
+            return View(orderVMList);
+        }
+    }
+}
+
+  
+
+
+    
