f10b692807c9db422fe4de50fccb4c644004b536
Geethakrishna44 <78337829+Geethakrishna44@users.noreply.github.com>
2021-02-12 06:04:11 -0800

    Day Eleven-11 task
    Create the text box, field box using Html helper for the front view

---
+++ Day11/ElevenController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DayEleven.Models;
+
+
+namespace DayEleven.Controllers
+{
+    public class ElevenController : Controller
+    {
+        // GET: Eleven
+        public ActionResult Index()
+        {
+            LovsanEntities1 db = new LovsanEntities1();
+            List<lovsan2> list = db.lovsan2.ToList();
+            ViewBag.Lovsan2list = new SelectList(list, "DepartmentID", "DepartmentName");
+
+            return View();
+        }
+    }
+}
---
+++ DayEleven11/EmployeeViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DayEleven.Models
+{
+    public class EmployeeViewModel
+    {
+
+        public int EmpID { get; set; }
+        public string EmpName { get; set; }
+        public int DepartmentID { get; set; }
+        public string Address { get; set; }
+
+        public bool DepartmentName { get; set; }
+
+
+
+    }
+}
