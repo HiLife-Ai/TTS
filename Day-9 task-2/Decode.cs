51ed762b0a10dcdd4b382a9fb74c76673b5e7b31
Priyanga1311 <78338138+Priyanga1311@users.noreply.github.com>
2021-02-12 15:34:01 +0530

    task 2 completed

---
+++ Day-9 task-2/Decode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Trust.Models
+{
+    public class Decode
+    {
+        public int COMPANY_ID { get; set; }
+        public string COMPANY_NAME { get; set; }
+        public string COMPANY_CITY { get; set; }
+
+        public string ITEM_NAME { get; set; }
+
+
+    }
+}
