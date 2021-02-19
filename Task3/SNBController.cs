60ddcf675fd2e9f5b1760dea70c014b87337dd35
Geethakrishna44 <78337829+Geethakrishna44@users.noreply.github.com>
2021-02-19 03:22:36 -0800

    Task3
    Side navigation bar for the project

---
+++ Task3/SNBController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Task3.Controllers
+{
+    public class SNBController : Controller
+    {
+        // GET: SNB
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
