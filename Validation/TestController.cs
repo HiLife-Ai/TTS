d759ee8ce2c4235fde52534ec4dc620f96fbdb69
Priyanga1311 <78338138+Priyanga1311@users.noreply.github.com>
2021-03-10 22:35:00 +0530

    Validation
    Client and Server side Validation

---
+++ Validation/TestController.cs
@@ -0,0 +1,41 @@
+﻿using Binding2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Binding2.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            RazorEntities1 db = new RazorEntities1();
+
+            List<Department> list = db.Departments.ToList();
+            ViewBag.DepartmentList = new SelectList(list, "DepartmentId", "DepartmentName");
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(ClassView1 model)
+        {
+            if (ModelState.IsValid == true)
+            {
+                
+                    //write your code here
+                
+            }
+            RazorEntities1 db = new RazorEntities1();
+
+            List<Department> list = db.Departments.ToList();
+            ViewBag.DepartmentList = new SelectList(list, "DepartmentId", "DepartmentName");
+
+
+            return View(model);
+
+        }
+    }
+}
