398801c410c3168eb7ef2df988f9389f34a60585
Priyanga1311 <78338138+Priyanga1311@users.noreply.github.com>
2021-03-10 21:22:29 +0530

    Create Form into Database

---
+++ repos/ClassView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Enter.Models
+{
+    public class ClassView
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public int DepartmentId { get; set; }
+        public string Address { get; set; }
+
+        //custom attribute
+        
+        public string DepartmentName  { get; set; }
+        public bool Remember { get; set; }
+
+    }
+}
---
+++ repos/TestController.cs
@@ -0,0 +1,23 @@
+﻿using Enter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Enter.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            RazorEntities db = new RazorEntities();
+
+            List<Department> list = db.Departments.ToList();
+            ViewBag.DepartmentList = new SelectList(list, "DepartmentId", "DepartmentName");
+
+            return View();
+        }
+    }
+}
