b785599cc144c2e73eb917ab0ed4ffd37dc2e6c2
Priyanga1311 <78338138+Priyanga1311@users.noreply.github.com>
2021-03-22 16:32:25 +0530

    create bootstrap popup(modal) and Insert data

---
+++ March/ModelBind3/ModelBind3/Controllers/TestController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ModelBind3.Models;
+
+namespace ModelBind3.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        [HttpGet]
+        public ActionResult Index()
+        {
+            KelvinEntities db = new KelvinEntities();
+
+            List<Department> list = db.Departments.ToList();
+            ViewBag.DepartmentList = new SelectList(list, "DepartmentId", "DepartmentName");
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(ViewClass model)
+        {
+            try
+            {
+
+                KelvinEntities db = new KelvinEntities();
+                List<Department> list = db.Departments.ToList();
+                ViewBag.DepartmentList = new SelectList(list, "DepartmentId", "DepartmentName");
+
+
+                Employee emp = new Employee();
+                emp.Address = model.Address;
+                emp.EmpName = model.EmpName;
+                emp.DepartmentId = model.DepartmentId;
+
+                db.Employees.Add(emp);
+                db.SaveChanges();
+
+                int latestEmpId = emp.EmpId;
+
+                Site site = new Site();
+                site.SName = model.SName;
+                site.EmpId = latestEmpId;
+
+                db.Sites.Add(site);
+                db.SaveChanges();
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ March/ModelBind3/ModelBind3/Models/ViewClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ModelBind3.Models
+{
+    public class ViewClass
+    {
+
+        public int EmpId { get; set; }
+
+        [Required (ErrorMessage="Enter Name")]
+        public string EmpName { get; set; }
+
+        [Required(ErrorMessage ="Enter Department")]
+        public int DepartmentId { get; set; }
+
+        [Required(ErrorMessage ="Enter Address")]
+        public string Address { get; set; }
+
+        //custom attribute...
+        public string DepartmentName { get; set; }
+        public bool Remember { get; set; }
+        public string SName { get; set; }
+    }
+}
