1422a64880fcaeb234cd9ef0cbb92dda1e6c2223
Priyanga1311 <78338138+Priyanga1311@users.noreply.github.com>
2021-03-22 16:27:25 +0530

    Inserting data into database using jQuery

---
+++ March/ModelBind2/ModelBind2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ModelBind2.MvcApplication" Language="C#" %>
---
+++ March/ModelBind2/ModelBind2/Models/ClassView.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ModelBind2.Models
+{
+    public class ClassView
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public int DepartmentId { get; set; }
+        public string Address { get; set; }
+
+        //custom attribute...
+        public string DepartmentName { get; set; }
+        public bool Remember { get; set; }
+        public string SName { get; set; }
+
+
+    }
+}
