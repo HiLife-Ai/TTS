f47efc480c236e8a499c46f67d6907d5e586d667
Priyanga1311 <78338138+Priyanga1311@users.noreply.github.com>
2021-03-22 16:22:12 +0530

    Insert data with multiple table

---
+++ March/ModelBind/ModelBind/Controllers/TestController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ModelBind.Models;
+
+namespace ModelBind.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        [HttpGet]
+        public ActionResult Index()
+        {
+            KelvinEntities db = new KelvinEntities();
+
+            List<Department> list = db.Departments.ToList();
+            ViewBag.DepartmentList = new SelectList(list, "DepartmentId", "DepartmentName");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(ViewModel model)
+        {
+           
+                KelvinEntities db = new KelvinEntities();
+                List<Department> list = db.Departments.ToList();
+                ViewBag.DepartmentList = new SelectList(list, "DepartmentId", "DepartmentName");
+
+                Employee emp = new Employee();
+                emp.Address = model.Address;
+                emp.EmpName = model.EmpName;
+                emp.DepartmentId = model.DepartmentId;
+
+                db.Employees.Add(emp);
+                db.SaveChanges();
+
+                int latestEmpId = emp.EmpId;
+
+
+                Site site = new Site();
+                site.SName = model.SName;
+                site.EmpId = latestEmpId;
+
+                db.Sites.Add(site);
+                db.SaveChanges();
+            
+           
+            return View(model);
+        }
+    }
+}
